b0c0854eeaf5cbb0ede9430565f17f9ffe612386
technolog <viktor@allz.su>
2018-12-05 02:38:15 +0300

    worker add appkey

--- worker.js
+++ worker.js
@@ -5,16 +5,18 @@ const scheduler = require("node-schedule");
 const jsonReader = require('jsonfile');
 const rConfig = jsonReader.readFileSync("./config.json");
 
-let url = 'http://127.0.0.1:' + rConfig.port + '/api/voters';
+let url = 'http://127.0.0.1:' + rConfig.port + '/api/voters/update';
 util.log("Worker started:" + 'http://127.0.0.1:' + rConfig.port);
 
 let workSchedule = scheduler.scheduleJob("*/10 * * * * *", () => {
     request({
-        method: 'get',
+        method: 'post',
         json: true, // Use,If you are sending JSON data
         url: url,
+        body: {},
         headers: {
-            "accept": "application/json"
+            "accept": "application/json",
+            "x-api-key": rConfig.appKey
         }
     }, function (err, res, body) {
         console.log(body);
