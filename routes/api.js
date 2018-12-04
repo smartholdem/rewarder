8dc8d85faac953f95850f032a9425cba8189bcaa
technolog <viktor@allz.su>
2018-12-05 01:17:53 +0300

    add level

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,8 @@ const sth = require("sthjs");
 const jsonReader = require('jsonfile');
 const util = require("../api/util");
 const rConfig = jsonReader.readFileSync("./config.json");
+const level = require("level");
+const db = level('.db', {valueEncoding: 'json'});
 
 if(!rConfig.secret)
 {
@@ -14,20 +16,22 @@ if(!rConfig.secret)
 
 const PUB_KEY = sth.crypto.getKeys(rConfig.secret).publicKey;
 
-
-/* GET users listing. */
+/* GET votes >= voterWeightMin */
 router.get('/voters', function(req, res, next) {
     let activeVoters = [];
+    let totalVote = 0;
     smartholdemApi.getVoters(PUB_KEY, (error, success, response) => {
         for (let i=0; i < response.accounts.length; i++) {
-            if (response.accounts[i].balance >= rConfig.voterWeightMin) {
+            if (response.accounts[i].balance / (10 ** 8) >= rConfig.voterWeightMin) {
+                totalVote = totalVote + response.accounts[i].balance / (10 ** 8);
                 activeVoters.push(response.accounts[i]);
             }
         }
-        res.json(activeVoters);
+        res.json({voters: activeVoters, total: totalVote});
     });
 });
 
+/* GET delegate info by name */
 router.get('/delegate/:name', function(req, res, next) {
         smartholdemApi.getDelegate(req.params["name"], (error, success, response) => {
             res.json(response);
