bb2b0fc2b9fd50d16066f954634c66dbe302a926
technolog <viktor@allz.su>
2018-12-05 06:45:18 +0300

    new payout

--- payouts.js
+++ payouts.js
@@ -7,19 +7,49 @@ const request = require("request");
 const jsonReader = require('jsonfile');
 const rConfig = jsonReader.readFileSync("./config.json");
 
-var doPayout = (options) => {
+var doPayout = () => {
     util.log("==Payout Begin==");
     util.log("DateTime: " + moment().toISOString());
-    let listVoters = [];
+    let transactions = [];
 
+    request({
+        method: 'get',
+        json: true, // Use,If you are sending JSON data
+        url: 'http://127.0.0.1:' + rConfig.port + '/api/voters/getFromDb',
+        headers: {
+            "accept": "application/json"
+        }
+    }, function (err, res, body) {
+        if (!err) {
+            let fee = 10000000;
+            for (let i=0; i < body.length; i++) {
+                let payoutSum = body[i].reward - fee;
+                if (payoutSum > 10000000) {
+                    transactions.push(smartholdemApi.createTransaction(rConfig.secret, body[i].address, payoutSum, {vendorField: "Payout from " + rConfig.delegate}));
+                }
+            }
 
+            console.log(transactions);
+            /*
+            smartholdemApi.sendTransactions(transactions, (error, success, responseSend) => {
+                if (!error) {
+                    if (responseSend.success === true) {
+                        for (let i=0; i < transactions.length; i++) {
+
+                        }
+                    }
+                }
+            });
+*/
+        }
+    });
 
 };
 
 exports.startScheduler = (cronJob, options) => {
     console.log(`Automatic payouts scheduled: ${cronJob}`);
-    console.log('options', options);
     let paySchedule = scheduler.scheduleJob(cronJob, () => {
+        util.log("Payout on DateTime: " + moment().toISOString());
         doPayout(options);
     });
 };
