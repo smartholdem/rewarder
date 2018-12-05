8b171f6c43217ca9cbf63ae5f3f246fa77f9c707
technolog <viktor@allz.su>
2018-12-05 05:13:14 +0300

    worker update

--- worker.js
+++ worker.js
@@ -5,20 +5,47 @@ const scheduler = require("node-schedule");
 const jsonReader = require('jsonfile');
 const rConfig = jsonReader.readFileSync("./config.json");
 
-let url = 'http://127.0.0.1:' + rConfig.port + '/api/voters/update';
 util.log("Worker started:" + 'http://127.0.0.1:' + rConfig.port);
 
 //let workSchedule = scheduler.scheduleJob("*/10 * * * * *", () => {
-    request({
-        method: 'post',
-        json: true, // Use,If you are sending JSON data
-        url: url,
-        body: {},
-        headers: {
-            "accept": "application/json",
-            "x-api-key": rConfig.appKey
-        }
-    }, function (err, res, body) {
-        console.log(body);
+
+function workerRequest(apiPath, callback) {
+
+        request({
+            method: 'post',
+            json: true, // Use,If you are sending JSON data
+            url: 'http://127.0.0.1:' + rConfig.port + apiPath,
+            body: {},
+            headers: {
+                "accept": "application/json",
+                "x-api-key": rConfig.appKey
+            }
+        }, function (err, res, body) {
+            if (!err) {
+                return callback(body);
+            } else {
+                return callback(false);
+            }
+        });
+
+}
+
+workerRequest('/api/worker/stats-update', function(dataStats){
+    workerRequest('/api/worker/voters-update', function(dataVoters){
+        console.log('dataStats',dataStats);
+        console.log('dataVoters',dataVoters);
+
+        request({
+            method: 'get',
+            json: true, // Use,If you are sending JSON data
+            url: 'http://127.0.0.1:' + rConfig.port + '/api/voters/getFromDb',
+            body: {},
+            headers: {
+                "accept": "application/json"
+            }
+        }, function (err, res, body) {
+            console.log(body);
+        });
+
     });
-//});
+});
