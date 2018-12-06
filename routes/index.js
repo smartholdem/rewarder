c1a4198e6e9a258da2e2e2e8607d6f58a83aad94
technolog <viktor@allz.su>
2018-12-06 03:15:40 +0300

    index

--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,19 @@ const smartholdemApi = require("sthjs-wrapper");
 const sth = require("sthjs");
 const request = require("request");
 
-const voterWeightMin = nconf.get("voterWeightMin") * 100000000;
-const PASSPHRASE = nconf.get("secret");
-const rewardPercent = nconf.get("rewardPercent");
-const voterDaysMin = nconf.get("voterDaysMin");
-
-if (!PASSPHRASE) {
+if (!rConfig.secret) {
     console.log("Please enter the SmartHoldem Delegate passphrase");
     process.exit(1);
 }
 
-const PUBKEY = sth.crypto.getKeys(PASSPHRASE).publicKey;
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'List Voters',
-        voters: listVoters,
-        total: total.toFixed(0),
-        minvote: voterWeightMin / 100000000,
-        voterDaysMin: voterDaysMin,
-        rewardpercent: rewardPercent,
-        delegate: delegateParams,
-        forgingMilestone: forgingMilestone,
-        distribution: distribution,
-        totalForged: delegateTotalForged,
-        nextReward: nextReward
+        title: 'Delegate Rewarder',
+        voterDaysMin: rConfig.voterDaysMin,
+        rewardPercent: rConfig.rewardPercent,
+        delegate: rConfig.delegate,
+        totalPayout: 0,
     });
 });
 
