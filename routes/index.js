1390b7417b577aa840697311c69815a8b8024b97
technolog <viktor@allz.su>
2018-12-14 21:42:03 +0300

    add delegate rank

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,24 @@ async function getStat() {
     });
 }
 
+async function getDelegate() {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'get',
+            json: true, // Use,If you are sending JSON data
+            url: 'http://localhost:' + rConfig.port + '/api/delegate/' + rConfig.delegate,
+            headers: {
+                "accept": "application/json"
+            }
+        }, function (err, res, body) {
+            if (!err) {
+                resolve(body);
+            }
+            reject(err);
+        });
+    });
+}
+
 async function getVoters() {
     return new Promise((resolve, reject) => {
         request({
@@ -48,25 +66,28 @@ router.get('/', function (req, res, next) {
     if (rConfig.personalPage) {
         getStat().then(function (dataStat) {
             getVoters().then(function (dataVoters) {
-                if (dataVoters.length > 0) {
-                    for (let i = 0; i < dataVoters.length; i++) {
-                        dataVoters[i].balance = dataVoters[i].balance / 100000000;
-                        dataVoters[i].reward = dataVoters[i].reward / 100000000;
+                getDelegate().then(function (dataDelegate) {
+                    if (dataVoters.length > 0) {
+                        for (let i = 0; i < dataVoters.length; i++) {
+                            dataVoters[i].balance = dataVoters[i].balance / 100000000;
+                            dataVoters[i].reward = dataVoters[i].reward / 100000000;
+                        }
                     }
-                }
 
-                let remainingHours = (Math.floor(Date.now() / 1000) - (Math.floor(dataStat.timestampUpdate / 1000) +  (rConfig.rewardPeriodDays * 60 * 60 * 24))) / 60 / 60;
+                    let remainingHours = (Math.floor(Date.now() / 1000) - ((Math.floor(dataStat.timestampUpdate / 1000) + (rConfig.rewardPeriodDays * 60 * 60 * 24))) / 60 / 60);
 
-                res.render('index', {
-                    title: 'Delegate ' + rConfig.delegate,
-                    stats: dataStat,
-                    voterDaysMin: rConfig.voterDaysMin,
-                    rewardPercent: rConfig.rewardPercent,
-                    delegate: rConfig.delegate,
-                    totalPayout: 0,
-                    voters: dataVoters,
-                    totalVoters: dataVoters.length,
-                    timeRemaining: remainingHours
+                    res.render('index', {
+                        title: 'Delegate ' + rConfig.delegate,
+                        stats: dataStat,
+                        voterDaysMin: rConfig.voterDaysMin,
+                        rewardPercent: rConfig.rewardPercent,
+                        delegate: rConfig.delegate,
+                        delegateInfo: dataDelegate.delegate,
+                        totalPayout: 0,
+                        voters: dataVoters,
+                        totalVoters: dataVoters.length,
+                        timeRemaining: remainingHours
+                    });
                 });
             });
         });
