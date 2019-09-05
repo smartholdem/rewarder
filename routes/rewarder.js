77cbc3dabc3c85020a9b53b02c4bb0dbe5cf2c41
technolog <technologies.bro@gmail.com>
2019-09-05 14:27:10 +0300

    class:Rewarder

--- routes/rewarder.js
+++ routes/rewarder.js
@@ -13,3 +13,20 @@ const jsonReader = require('jsonfile')
 const rConfig = jsonReader.readFileSync("./config.json")
 const level = require("level")
 const db = level('./.rewarder', {valueEncoding: 'json'})
+
+// 0x - Voters
+// 1x - Stats
+// 2x - Payouts
+
+if (!rConfig.secret) {
+    util.log("Please enter the SmartHoldem Delegate passphrase");
+    process.exit(1);
+}
+
+const PUB_KEY = sth.crypto.getKeys(rConfig.secret).publicKey;
+
+class Rewarder {
+    async statsUpdate() {
+
+    }
+}
