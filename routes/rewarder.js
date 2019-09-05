e72682af7b49dd05805df772e7e701b4ba909a2b
technolog <technologies.bro@gmail.com>
2019-09-05 14:47:23 +0300

    compareValues

--- routes/rewarder.js
+++ routes/rewarder.js
@@ -25,6 +25,31 @@ if (!rConfig.secret) {
 
 const PUB_KEY = sth.crypto.getKeys(rConfig.secret).publicKey;
 
+// function for dynamic sorting
+function compareValues(key, order = 'asc') {
+    return function (a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            return 0;
+        }
+
+        const varA = (typeof a[key] === 'string') ?
+            a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string') ?
+            b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return (
+            (order == 'desc') ? (comparison * -1) : comparison
+        );
+    };
+}
+
 class Rewarder {
 
     async statsUpdate(PubKey) {
