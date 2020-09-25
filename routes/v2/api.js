8cddc705cc70aaebe94695c2b335a8f75981fe53
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-09-25 23:27:56 +0300

    dbArray

--- routes/v2/api.js
+++ routes/v2/api.js
@@ -427,8 +427,8 @@ router.get('/total-info', async function (req, res, next) {
     let result = {
         delegate: await dbUtils.dbGet(db, 'DELEGATE'),
         voters: {
-            pending: await dbUtils.dbObj(db, '0', '1'),
-            active: await dbUtils.dbObj(db, '1', '2'),
+            pending: await dbUtils.dbArray(db, '0', '1'),
+            active: await dbUtils.dbArray(db, '1', '2'),
         }
     }
     res.json(result)
