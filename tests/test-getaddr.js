10490d07d6ce9bca3e905032f3aa8ca38ecb30e6
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-09-26 17:32:55 +0300

    delete old test

--- tests/test-getaddr.js
+++
@@ -1,8 +0,0 @@
-const jsonReader = require('jsonfile');
-const rConfig = jsonReader.readFileSync("./config.json");
-const sth = require("sthjs");
-const PUB_KEY = sth.crypto.getKeys(rConfig.secret).publicKey;
-const ADDRESS = sth.crypto.getAddress(PUB_KEY);
-
-console.log('PUB_KEY',PUB_KEY);
-console.log('ADDRESS',ADDRESS);
